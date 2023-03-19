12c8ce1d66a840c9ac66834e70308c91c0e7379e
Wenyi Yin <wenyi.yin@mail.utoronto.ca>
2023-03-19 01:24:58 -0400

    turn on debug

--- keyboards/nuthouse/bananasplit/keymaps/yinwenyi/keymap.c
+++ keyboards/nuthouse/bananasplit/keymaps/yinwenyi/keymap.c
@@ -122,3 +122,9 @@ bool dip_switch_update_user(uint8_t index, bool active) {
 
    return true;
 }
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+}
