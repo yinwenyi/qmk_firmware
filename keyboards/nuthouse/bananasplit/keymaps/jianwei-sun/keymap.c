3099d2f912a84c42b514e9eb142c6e3302334f00
Jianwei Sun <jian58sun@gmail.com>
2024-08-21 19:59:01 -0700

    Updated keymap for macos and added some macros

--- keyboards/nuthouse/bananasplit/keymaps/jianwei-sun/keymap.c
+++ keyboards/nuthouse/bananasplit/keymaps/jianwei-sun/keymap.c
@@ -20,16 +20,22 @@ enum layer_names {
     _MAIN
 };
 
+enum custom_keycodes {
+    CMD_LEFT = SAFE_RANGE,
+    CMD_RIGHT,
+    LOCK_SCREEN
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MAIN] = LAYOUT(
   //┌────────┬────────┬────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┐      ┌────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┬────────┬────────┐
-     KC_PSCR, KC_PGUP, KC_PGDN,  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,  KC_RBRC, KC_HOME, KC_MINS,
+     KC_PSCR, KC_PGUP, KC_INS ,  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,  KC_RBRC, CMD_LEFT, KC_MINS,
   //├────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤└────────┘      └────────┘├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┤
-     KC_INS,  KC_BSLS, KC_PMNS,  KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_DEL,  KC_END,  KC_EQL,
+LOCK_SCREEN,  KC_PGDN, KC_BSLS,  KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_DEL,  CMD_RIGHT, KC_EQL,
   //├────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┤
               KC_CAPS,           KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_UP,
   //├────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┤
-     KC_LCMD, KC_LALT, KC_LCTL,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_LEFT, KC_DOWN, KC_RIGHT,
+     KC_LCTL, KC_LALT, KC_LCMD,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_LEFT, KC_DOWN, KC_RIGHT,
   //└────────┴────────┴────────┘└────────┴────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┴────────┴────────┴────────┘└────────┴────────┴────────┘
                                                                               KC_SPC,  KC_ENT,           KC_ENT,  KC_SPC
   //                                                                         └────────┴────────┘        └────────┴────────┘
@@ -84,3 +90,28 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
     return true;
 }
+
+// Implement the custom macro logic
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case CMD_LEFT:{
+            if(record->event.pressed){
+                SEND_STRING(SS_LCMD(SS_TAP(X_LEFT)));
+            }
+            break;
+        }
+        case CMD_RIGHT:{
+            if(record->event.pressed){
+                SEND_STRING(SS_LCMD(SS_TAP(X_RIGHT)));
+            }
+            break;
+        }
+        case LOCK_SCREEN:{
+            if(record->event.pressed){
+                SEND_STRING(SS_LCMD(SS_LCTL(SS_TAP(X_Q))));
+            }
+            break;
+        }
+    }
+    return true;
+};
